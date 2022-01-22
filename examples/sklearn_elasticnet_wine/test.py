24b1bd6998005048f7f63077a7ac5c8b41203e89
Ei.Yamaguchi@jp.nttdata.com <Ei.Yamaguchi@jp.nttdata.com>
2022-01-22 04:37:02 +0000

    asaaa
    asasa

--- examples/sklearn_elasticnet_wine/test.py
+++ examples/sklearn_elasticnet_wine/test.py
@@ -2,7 +2,18 @@
 if __name__ == '__main__':
     print("Start if __name__ == '__main__'")
     print('call func()')
-    print(sc)
+    #print(sc)
     print(spark)
-    sc
+    #sc
     spark
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+from pyspark import SparkContext
+from pyspark.sql import SQLContext
+
+spark
